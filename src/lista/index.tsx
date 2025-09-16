57a1cb9155cd056665aa5bf74b16a6a6be7ecf28
Renato Silva <renatoef79@gmail.com>
2025-09-16 19:58:39 -0300

    projeto inicial

---
+++ src/lista/index.tsx
@@ -0,0 +1,9 @@
+const Cabecalho = () => (
+  <form>
+    <input type="text" placeholder="nome completo" />
+    <input type="email" placeholder="email" />
+    <input type="number" placeholder="número de telefone" />
+  </form>
+);
+
+export default Cabecalho;
